afbb28a6d376c5ec5942c59f8df7107d30b3a153
Affan <affuafk123@gmail.com>
2025-10-31 20:15:39 +0530

    lasthost

--- Frontend/src/apicall/axios.js
+++ Frontend/src/apicall/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/", // Django API base URL
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/",
 });
 
 // Attach token automatically if available
