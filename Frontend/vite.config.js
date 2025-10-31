ac4b8a66c9e645ef82299234eaac56987ce158ab
Affan <affuafk123@gmail.com>
2025-10-31 20:23:31 +0530

    distcheck

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   build: {
+    outDir: 'dist', // 👈 this tells Vercel where to find the build files
+  },
   
 })
